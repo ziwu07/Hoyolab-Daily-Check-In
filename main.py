bd91de6914b7352fa061f10b379e0c6a32ec283d
ziwu07 <mzw7611@gmail.com>
2023-07-27 16:25:55 -0400

    if ('opera' in brower) and ('gx' in brower)

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ _config = config.read_config()
 
 def GetToken(browser:str, domain_name:str):
     try:
-        if browser.lower == 'all':
+        if browser.lower() == 'all':
             cookies = browser_cookie3.load(domain_name=domain_name)
-        elif browser.lower == 'firefox':
+        elif browser.lower() == 'firefox':
             cookies = browser_cookie3.firefox(domain_name=domain_name)
-        elif browser.lower == 'chrome':
+        elif browser.lower() == 'chrome':
             cookies = browser_cookie3.chrome(domain_name=domain_name)
-        elif browser.lower == 'opera':
+        elif browser.lower() == 'opera':
             cookies = browser_cookie3.opera(domain_name=domain_name)
-        elif browser.lower == 'edge':
+        elif browser.lower() == 'edge':
             cookies = browser_cookie3.edge(domain_name=domain_name)
-        elif browser.lower == 'chromium':
+        elif browser.lower() == 'chromium':
             cookies = browser_cookie3.chromium(domain_name=domain_name)
-        elif browser.lower == 'operagx' or browser.lower == 'opera_gx':
+        elif ('opera' in browser) and ('gx' in browser):
             cookies = browser_cookie3.opera_gx(domain_name=domain_name)
         else:
             error.show_error_message('no browser defined')
